0ddb7740d4a894726132c7589194b1a92527d1d1
ivanochoa229 <ivanochoa229@gmail.com>
2025-06-03 09:59:04 -0300

    se creo el backend de moroscan

---
+++ src/main/java/com/moroapp/presentation/dto/response/ProductionResponse.java
@@ -0,0 +1,10 @@
+package com.moroapp.presentation.dto.response;
+
+
+public record ProductionResponse(Long id_production, int quantity_plots,
+                                 Double expected_production,
+                                 Double lost_production,
+                                 Double real_production,
+                                 String date_simulation,
+                                 int spring_type) {
+}
